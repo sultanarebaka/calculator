9eaeb39a8233087d25a4786607cde1b0802ce710
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-07-14 19:39:51 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const display = document.querySelector(".display");
-
 const buttons = document.querySelectorAll("button");
-
 const specialChars = ["%", "*", "/", "-", "+", "="];
 
 let output = "";
@@ -9,17 +7,13 @@ let output = "";
 //Define
 
 const calculate = (btnValue) => {
-
 display.focus();
-
 if (btnValue === "=" && output !== "") {
 
 
 
 output = eval(output.replace("%", "/100"));
-
 } else if (btnValue === "AC") {
-
 output = "";
 
 } else if (btnValue === "DEL") {
@@ -33,20 +27,16 @@ output = output.toString().slice(0, -1);
 //return
 
 if (output === "" && specialChars.includes(btnValue)) return;
-
 output += btnValue;
 
 }
 
 display.value = output;
-
 };
 
 
 
 buttons.forEach((button) => {
-
-
 button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 
 });
